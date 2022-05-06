081286b3abf9b8adb19b3120e07fbcca98d80ec1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-07 08:43:42 +0900

    scriptの編集

--- vue3-typescript/vue-sample/src/components/HelloWorld.vue
+++ vue3-typescript/vue-sample/src/components/HelloWorld.vue
@@ -4,6 +4,9 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const plusCount = () => {
+  count.value++
+}
 </script>
 
 <template>
@@ -26,7 +29,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="plusCount">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
