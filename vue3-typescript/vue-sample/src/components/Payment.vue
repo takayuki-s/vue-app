34d7bdd6f27f7e8afba23afef4e62771e2f0c298
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-07 09:27:23 +0900

    商品名を変数化

--- vue3-typescript/vue-sample/src/components/Payment.vue
+++ vue3-typescript/vue-sample/src/components/Payment.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+  const itemName1 = "Desk"
+  const itemName2 = "Bike"
+
+  const price1 = 40000
+  const price2 = 20000
 </script>
 
 <template>
 <div class="container">
   <h1>最近の支出</h1>
   <div class="payment">
-    <label>カレーライス</label>
-    <label>400円</label>
+    <label>{{ itemName1 }}</label>
+    <label>{{ price1 }} yen</label>
+  </div>
+  <div class="payment">
+    <label>{{ itemName2 }}</label>
+    <label>{{ price2 }} yen</label>
   </div>
 </div>
 </template>
@@ -24,6 +33,7 @@
     height: 80px;
     width: 400px;
     background-color: aliceblue;
+    margin-bottom: 8px;
   }
   label {
     font-size: 20px;
