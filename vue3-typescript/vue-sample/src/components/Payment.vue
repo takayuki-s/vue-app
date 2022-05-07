c53a2cde2cc8a692d64bb131aab7649f45b7e1de
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-08 08:53:32 +0900

    v-on処理を使った購入ボタン

--- vue3-typescript/vue-sample/src/components/Payment.vue
+++ vue3-typescript/vue-sample/src/components/Payment.vue
@@ -7,6 +7,10 @@
 
   const url1 = "https://flexispot.jp/standing-desk/e9.html?gclid=CjwKCAjwjtOTBhAvEiwASG4bCE_eT44g8UjiYePQaQm9kCXldYW8_Mqp87KR2S5ocYsMKZPLjocVFxoCm3EQAvD_BwE"
   const url2 = "https://item.rakuten.co.jp/55cycling/4571487004936/?scid=s_kwa_pla_unpaid_201689"
+
+  const buy = (itemName: string) => {
+    alert("Are you sure to buy " + itemName + "?")
+  }
 </script>
 
 <template>
@@ -16,11 +20,13 @@
     <label>{{ itemName1 }}</label>
     <label>{{ price1 }} yen</label>
     <a v-bind:href="url1">bought at...</a>
+    <button v-on:click="buy(itemName1)">BUY</button>
   </div>
   <div class="payment">
     <label>{{ itemName2 }}</label>
     <label>{{ price2 }} yen</label>
     <a v-bind:href="url2">bought at...</a>
+    <button v-on:click="buy(itemName2)">BUY</button>
   </div>
 </div>
 </template>
