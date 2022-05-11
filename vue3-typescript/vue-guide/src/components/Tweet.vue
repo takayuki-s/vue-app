d0fc968be37fa10e6aad699456062ef76989f582
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-11 17:38:21 +0900

    動的リスト作成練習用ファイル

---
+++ vue3-typescript/vue-guide/src/components/Tweet.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+</style>
