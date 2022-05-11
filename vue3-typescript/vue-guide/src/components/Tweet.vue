f834f6376caafd377ad86d417da3e8996523350d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-12 05:30:56 +0900

    静的なリストを表示

--- vue3-typescript/vue-guide/src/components/Tweet.vue
+++ vue3-typescript/vue-guide/src/components/Tweet.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+const tweets = [{ id: "0", description: "Hello, world!"}, { id: "1", description: "this is a second tweet"}]
 </script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input />
+      <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -21,5 +35,27 @@
   width: 60%;
   margin-bottom: 12px;
   border-radius: 4px;
+}
+
+.tweet-list {
+  list-style: none;
+}
+
+button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
+}
+
+button:hover {
+  margin-bottom: 16px
+}
+
+input {
+  margin-bottom: 16px;
 }
 </style>
