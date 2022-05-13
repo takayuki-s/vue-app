9673cd80326e40f605dd9281e82453d4f205042d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-13 09:16:55 +0900

    コンポーネント切り分け
    メイン、inputフォーム、リストに分ける

---
+++ vue3-typescript/vue-guide/src/components/TweetList.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const tweets: any = []
+const deleteTweet = (id: number) => {}
+</script>
+
+<template>
+  <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+    <span>{{ tweet.description }}</span>
+    <button @click="deleteTweet(tweet.id)" class="delete-button">delete</button>
+  </li>
+</template>
+
+<style scoped>
+.tweet-list {
+  list-style: none;
+  margin-bottom: 12px;
+  border-radius: 4px;
+  font-size: 12px;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(204, 219, 233);
+  padding: 8px 20px;
+  width: 300px;
+}
+
+.delete-button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #c99a68;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
+}
+.delete-button:hover {
+  background-color: #ac783f;
+}
+</style>
