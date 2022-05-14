055321e431bcad073c04aa6b6b3be097cf1f8c2a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-15 07:21:29 +0900

    全体構成を表示

--- vue3-typescript/vue-guide2/src/App.vue
+++ vue3-typescript/vue-guide2/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import Persons from './components/Persons.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template></template>
+<template>
+  <Persons />
+</template>
 
 <style>
 #app {
---
+++ vue3-typescript/vue-guide2/src/components/Persons.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, Ref } from "vue"
+import ParsonPostForm from './ParsonPostForm.vue';
+import PersonList from './PersonList.vue';
+
+export type Person = {
+  id: number,
+  name: string,
+  age: number,
+}
+
+const persons: Ref<Person[]> = ref([{id: 0, name: "John", age: 24}, {id: 1, name: "Mike", age: 26}])
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>title</h1>
+    <ParsonPostForm />
+    <div class="list-container">
+      <ul>
+        <PersonList :persons="persons" />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
