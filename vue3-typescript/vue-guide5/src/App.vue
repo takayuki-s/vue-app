00f3427acd46aff907399a5f822ee39f0d9cb2f2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-18 07:57:09 +0900

    動的なコンポーネントを使ったタブ切り換え

--- vue3-typescript/vue-guide5/src/App.vue
+++ vue3-typescript/vue-guide5/src/App.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import CardList from './components/CardList.vue';
 import SecondCardList from './components/SecondCardList.vue';
 
 // const isFirstTab = ref(true)
-// const updateTab = (isFirst: boolean) => {
-//   isFirstTab.value = isFirst
-// }
+const isActiveTabName = ref("1")
+const updateTab = (activeName: string) => {
+  isActiveTabName.value = activeName
+}
+
+const currentComponent = computed(() => {
+  switch(isActiveTabName.value) {
+    case "1":
+      return CardList
+    case "2":
+      return SecondCardList
+    case "3":
+      return CardList
+    default:
+      return CardList
+  }
+})
 
 </script>
 
 <template>
-  <!-- <div class="tab-changer">
-    <button @click="updateTab(true)">Tab 1</button>
-    <button @click="updateTab(false)">Tab 2</button>
-  </div> -->
+  <div class="tab-changer">
+    <button @click="updateTab('1')">Tab 1</button>
+    <button @click="updateTab('2')">Tab 2</button>
+    <button @click="updateTab('3')">Tab 3</button>
+  </div>
   <div class="tab-contents">
-    <CardList></CardList>
+    <!-- <CardList></CardList> -->
+    <component :is="currentComponent"></component>
     <!-- <CardList v-if="isFirstTab"></CardList>
     <SecondCardList v-if="!isFirstTab"></SecondCardList> -->
   </div>
