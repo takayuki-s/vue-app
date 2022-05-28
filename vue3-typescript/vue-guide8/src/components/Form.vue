4ada55d64249e1bd72b32c4da5f7f576559bf969
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-28 11:58:55 +0900

    データ取得中はLoadingを表示

--- vue3-typescript/vue-guide8/src/components/Form.vue
+++ vue3-typescript/vue-guide8/src/components/Form.vue
@@ -10,9 +10,13 @@ const vFocus = {
 
 const userName = ref<string>('')
 const interest = ref([])
+const data = ref()
+const isLoading = ref<boolean>(false)
 
 onMounted(async () => {
-  const data = await axios.get("URL")
+  isLoading.value = true
+  data.value = await axios.get("URL")
+  isLoading.value = false
   console.log("data is ", data)
 })
 
@@ -53,6 +57,10 @@ const onSubmit = (e: Event) => {
         />
         <label for="interest-angular">Angular.js</label>
       </div>
+    </div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      {{ data }}
     </div>
     <div>
       <button @click.prevent="onSubmit">Save Data</button>
