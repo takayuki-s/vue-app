7099311ece1766fe8fca6583d8aa84bfc1199f64
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-05-29 09:56:59 +0900

    エラーハンドリング

--- vue3-typescript/vue-guide8/src/components/Form.vue
+++ vue3-typescript/vue-guide8/src/components/Form.vue
@@ -12,10 +12,19 @@ const userName = ref<string>('')
 const interest = ref([])
 const data = ref()
 const isLoading = ref<boolean>(false)
+const err = ref()
 
 onMounted(async () => {
   isLoading.value = true
-  data.value = await axios.get("URL")
+  try {
+    const response = await axios.get("URL")
+    if (response.status !== 200) {
+      throw new Error ("サーバー側のエラー")
+    }
+    data.value = response
+  } catch(e) {
+    err.value = e
+  }
   isLoading.value = false
   console.log("data is ", data)
 })
@@ -62,6 +71,7 @@ const onSubmit = (e: Event) => {
     <div v-else>
       {{ data }}
     </div>
+    <div v-if="err">{{ err }}</div>
     <div>
       <button @click.prevent="onSubmit">Save Data</button>
     </div>
